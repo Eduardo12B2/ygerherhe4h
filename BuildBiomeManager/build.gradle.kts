95a72cc8058d30366472e0a39c2c1badea4ab13b
Eduardo12B <109172340+TheEternalDark@users.noreply.github.com>
2026-03-06 20:25:25 -0300

    d
    sds

---
+++ BuildBiomeManager/build.gradle.kts
@@ -0,0 +1,66 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription.PluginLoadOrder.STARTUP
+
+group = "me.cjcrafter"
+version = "3.7.3"
+
+plugins {
+    `java-library`
+    id("io.github.goooler.shadow") version "8.1.7"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+}
+
+// See https://github.com/Minecrell/plugin-yml
+bukkit {
+    main = "me.cjcrafter.biomemanager.BiomeManager"
+    name = "BiomeManager"
+    apiVersion = "1.16"
+    load = STARTUP // required to register biomes before world load
+
+    authors = listOf("CJCrafter")
+    depend = listOf("ProtocolLib", "MechanicsCore")
+    softDepend = listOf("TerraformGenerator")  // softdepend on plugins that register custom biomes so we can modify them
+    loadBefore = listOf("WorldEdit")
+}
+
+repositories {
+    mavenCentral() // shade bStats
+}
+
+dependencies {
+    implementation(project(":"))
+    implementation(project(":Biome_1_19_R3", "reobf"))
+    implementation(project(":Biome_1_20_R1", "reobf"))
+    implementation(project(":Biome_1_20_R2", "reobf"))
+    implementation(project(":Biome_1_20_R3", "reobf"))
+    implementation(project(":Biome_1_20_R4", "reobf"))
+    implementation(project(":Biome_1_21_R1", "reobf"))
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.shadowJar {
+    destinationDirectory.set(file("../build"))
+    archiveFileName.set("BiomeManager-${project.version}.jar")
+
+    dependencies {
+        include(project(":"))
+        include(project(":Biome_1_19_R3"))
+        include(project(":Biome_1_20_R1"))
+        include(project(":Biome_1_20_R2"))
+        include(project(":Biome_1_20_R3"))
+        include(project(":Biome_1_20_R4"))
+        include(project(":Biome_1_21_R1"))
+
+        relocate("org.bstats", "me.cjcrafter.biomemanager.lib.bstats") {
+            include(dependency("org.bstats:"))
+        }
+    }
+
+    // This doesn't actually include any dependencies, this relocates all references
+    // to the mechanics core lib.
+    relocate("net.kyori", "me.deecaad.core.lib")
+}
---
+++ src/main/java/me/cjcrafter/biomemanager/BiomeRegistry.java
@@ -0,0 +1,101 @@
+package me.cjcrafter.biomemanager;
+
+import com.comphenix.protocol.collections.IntegerMap;
+import me.cjcrafter.biomemanager.compatibility.BiomeCompatibilityAPI;
+import me.cjcrafter.biomemanager.compatibility.BiomeWrapper;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Biome;
+import org.bukkit.plugin.Plugin;
+
+import java.util.*;
+
+public final class BiomeRegistry {
+
+    // Singleton design pattern
+    private static final BiomeRegistry INSTANCE = new BiomeRegistry();
+
+    public static BiomeRegistry getInstance() {
+        return INSTANCE;
+    }
+
+
+    private final Map<NamespacedKey, BiomeWrapper> map;
+    private final IntegerMap<BiomeWrapper> byId;
+    private final Set<NamespacedKey> removedBiomes;
+
+    public BiomeRegistry() {
+        map = new LinkedHashMap<>();
+        byId = new IntegerMap<>();
+        removedBiomes = new HashSet<>();
+    }
+
+    public void add(Plugin plugin, String key, BiomeWrapper biome) {
+        add(new NamespacedKey(plugin, key), biome);
+    }
+
+    public void add(NamespacedKey key, BiomeWrapper biome) {
+        if (map.containsKey(key))
+            throw new IllegalArgumentException("Duplicate key '" + key + "'");
+        if (removedBiomes.contains(key))
+            throw new IllegalArgumentException("Tried to add deleted biome '" + key + "' without restarting the server");
+
+        map.put(key, biome);
+        byId.put(biome.getId(), biome);
+    }
+
+    public BiomeWrapper remove(NamespacedKey key) {
+        BiomeWrapper removed = map.remove(key);
+        if (removed != null)
+            removedBiomes.add(key);
+        return removed;
+    }
+
+    public BiomeWrapper get(NamespacedKey key) {
+        return map.get(key);
+    }
+
+    public BiomeWrapper getById(int id) {
+        return byId.get(id);
+    }
+
+    public BiomeWrapper getBukkit(Biome biome) {
+        if (biome == Biome.CUSTOM)
+            throw new IllegalArgumentException("Cannot use Biome.CUSTOM");
+
+        BiomeWrapper wrapper = map.get(biome.getKey());
+        if (wrapper == null)
+            throw new IllegalStateException("BiomeManager failed to wrap vanilla biomes... map: " + map);
+
+        return wrapper;
+    }
+
+    public BiomeWrapper getOrCreate(NamespacedKey key) {
+        return getOrCreate(key, Biome.PLAINS);
+    }
+
+    public BiomeWrapper getOrCreate(NamespacedKey key, Biome base) {
+        return getOrCreate(key, getBukkit(base));
+    }
+
+    public BiomeWrapper getOrCreate(NamespacedKey key, BiomeWrapper base) {
+        BiomeWrapper biome = get(key);
+        if (biome == null) {
+            biome = BiomeCompatibilityAPI.getBiomeCompatibility().createBiome(key, base);
+            add(key, biome);
+        }
+        return biome;
+    }
+
+    public Set<NamespacedKey> getKeys(boolean dirty) {
+        if (dirty) {
+            Set<NamespacedKey> temp = new LinkedHashSet<>();
+            for (Map.Entry<NamespacedKey, BiomeWrapper> entry : map.entrySet()) {
+                if (entry.getValue().isDirty())
+                    temp.add(entry.getKey());
+            }
+            return temp;
+        }
+
+        return Collections.unmodifiableSet(map.keySet());
+    }
+}
